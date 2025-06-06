bcbe6df32e4f74ee15335e104a3a3f1cdb421ac6
Your Name <lucascampanharo19@gmail.com>
2025-06-06 12:55:35 -0300

    arrumado permissões

--- blocks/scientific_calc/db/access.php
+++ blocks/scientific_calc/db/access.php
@@ -1,12 +1,33 @@
 <?php
-$capabilities = [
-    'block/scientificcalc:addinstance' => [
+
+$capabilities = array(
+
+    'block/seubloquinho:addinstance' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
-        'archetypes' => [
+        'archetypes' => array(
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
-        ],
-    ],
-];
-?>
+        )
+    ),
+
+    'block/seubloquinho:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        )
+    ),
+
+    // 👇 ESTA É A PERMISSÃO QUE FAZ OS ALUNOS VEREM O BLOCO
+    'block/seubloquinho:view' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    )
+);
